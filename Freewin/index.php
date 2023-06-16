8466de9a8194fc1489e2a5f24e3c893176022d2f
SwannMartineau <swann.martineau@gmail.com>
2023-06-16 05:03:20 +0200

    responsive

--- Freewin/index.php
+++ Freewin/index.php
@@ -15,6 +15,7 @@
 ?>
 
 <html>
+
 <head>
     <title>Freewin -- La victoire entre vos mains!</title>
     <link rel="stylesheet" href="css/index.css" type="text/css" />
@@ -22,139 +23,136 @@
     <script type="text/javascript" src="librairy\Winwheel.js"></script>
     <script src="javascript/ajax/ajax.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 
 <body>
+   
     <div class="wrapper">
+
         <div class="head one">
             <h1 class="title">Freewin</h1>
-            <p class="description">
-                La victoire entre vos mains !
-            </p>
+            <p class="description">La victoire entre vos mains !</p>
+        </div>
+
+        <div class="game two">
+            <table>
+                <tr>
+        <!---------------------------- Wheel n°1 ---------------------------->
+                    <td> 
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <img id="fleche" src="images/fleche.png" width="50" height="50"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="canvas">
+                                            <canvas id="canvas1" width="434" height="434" data-responsiveMinWidth="180" data-responsiveScaleHeight="true" data-responsiveMargin="50">
+                                                <p style="color: white"> Sorry, your browser doesn't support canvas. Please try another.</p>
+                                            </canvas>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class = center>
+                                            <button id="btn_spin_1" hidden class="btn button play" onClick="playPub('btn_spin_1')"> Tournez la roue ! </button>
+                                            <span id="timer_1" class="text-time"></span>
+                                            <!-- <input type="button" id="button_play" hidden value="Jouer"> -->
+                                            <!-- <img id="spin_button"  src="images\spin.png" width="50" height="50" alt="Spin" onClick="resetWheel(1); return false;" /> -->
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+            <!---------------------------- Wheel n°2 ----------------------------> 
+                    <td>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <img id="fleche" src="images/fleche.png" width="50" height="50"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="canvas">
+                                            <canvas id="canvas2" width="434" height="434">
+                                                <p style="color: white"> Sorry, your browser doesn't support canvas. Please try another.</p>
+                                            </canvas>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class = center>
+                                            <button id="btn_spin_2" hidden class="btn button play" onClick="playPub('btn_spin_2');"> Tournez la roue ! </button>
+                                            <span id="timer_2" class="text-time"></span>
+                                            <!-- <img id="spin_button"  src="images\spin.png" width="50" height="50" alt="Spin" onClick="resetWheel(2); return false;" /> -->
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+            <!---------------------------- Wheel n°3 ----------------------------> 
+                    <td>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <img id="fleche" src="images/fleche.png" width="50" height="50"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="canvas">
+                                            <canvas id="canvas3" width="434" height="434">
+                                                <p style="color: white"> Sorry, your browser doesn't support canvas. Please try another.</p>
+                                            </canvas>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class = center>
+                                            <button id="btn_spin_3" hidden class="btn button play" onClick="playPub('btn_spin_3');"> Tournez la roue ! </button>
+                                            <span id="timer_3" class="text-time"></span>
+                                            <!--<input type="button" id="button_play" hidden value="Jouer">
+                                            <img id="spin_button"  src="images\spin.png" width="50" height="50" alt="Spin" onClick="resetWheel(3); return false;" /> -->
+                                        </div> 
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </table>
         </div>
     
-    <div class="game wrapper">
-        <table>
-            <tr>
-    <!---------------------------- Wheel n°1 ---------------------------->
-                <td>
-                <div class="two"> 
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <img id="fleche" src="images/fleche.png" width="50" height="50"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="canvas">
-                                        <canvas id="canvas1" width="434" height="434" data-responsiveMinWidth="180" data-responsiveScaleHeight="true" data-responsiveMargin="50">
-                                            <p style="color: white"> Sorry, your browser doesn't support canvas. Please try another.</p>
-                                        </canvas>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class = center>
-                                        <button id="btn_spin_1" hidden class="btn button play" onClick="playPub('btn_spin_1')"> Tournez la roue ! </button>
-                                        <span id="timer_1" class="text-time"></span>
-                                        <!-- <input type="button" id="button_play" hidden value="Jouer"> -->
-                                        <!-- <img id="spin_button"  src="images\spin.png" width="50" height="50" alt="Spin" onClick="resetWheel(1); return false;" /> -->
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    </div>  
-                </td>
         
-        <!---------------------------- Wheel n°2 ----------------------------> 
-        <div class="three">
-                <td>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <img id="fleche" src="images/fleche.png" width="50" height="50"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="canvas">
-                                        <canvas id="canvas2" width="434" height="434">
-                                            <p style="color: white"> Sorry, your browser doesn't support canvas. Please try another.</p>
-                                        </canvas>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class = center>
-                                        <button id="btn_spin_2" hidden class="btn button play" onClick="playPub('btn_spin_2');"> Tourner la roue! </button>
-                                        <span id="timer_2" class="text-time"></span>
-                                        <!-- <img id="spin_button"  src="images\spin.png" width="50" height="50" alt="Spin" onClick="resetWheel(2); return false;" /> -->
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
+    </div>
+    <div id="popup">
+        <div id="videoContainer">
+        <video id="videoPlayer" autoplay="true" controls>
+            <source id="src_video" src="" type="video/mp4">
+        </video>
         </div>
-        <!---------------------------- Wheel n°3 ----------------------------> 
-        <div class="four">
-                <td>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <img id="fleche" src="images/fleche.png" width="50" height="50"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="canvas">
-                                        <canvas id="canvas3" width="434" height="434">
-                                            <p style="color: white"> Sorry, your browser doesn't support canvas. Please try another.</p>
-                                        </canvas>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class = center>
-                                        <button id="btn_spin_3" hidden class="btn button play" onClick="playPub('btn_spin_3');"> Tourner la roue! </button>
-                                        <span id="timer_3" class="text-time"></span>
-                                        <!--<input type="button" id="button_play" hidden value="Jouer">
-                                        <img id="spin_button"  src="images\spin.png" width="50" height="50" alt="Spin" onClick="resetWheel(3); return false;" /> -->
-                                    </div> 
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-            </tr>
-        <div>
-    </div>   
-<div id="popup">
-    <div id="videoContainer">
-      <video id="videoPlayer" autoplay="true" controls>
-        <source id="src_video" src="" type="video/mp4">
-      </video>
     </div>
-  </div>
 
-<div id="alerte" class="alerte-victoire">
-    <div id="alerte-contenu" class="alerte-contenu">
-        <p id="alerte-paragraphe" class="alerte-paragraphe"></p>
-        <button class="btn button play" onclick="fermerAlerteVictoire()">Fermer</button>
+    <div id="alerte" class="alerte-victoire">
+        <div id="alerte-contenu" class="alerte-contenu">
+            <p id="alerte-paragraphe" class="alerte-paragraphe"></p>
+            <button class="btn button play" onclick="fermerAlerteVictoire()">Fermer</button>
+        </div>
     </div>
-</div>
-<script type="text/javascript" src="librairy/index.js"></script>
 </body>
-<script>changeHour()</script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script>changeHour()</script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script type="text/javascript" src="librairy/index.js"></script>
 </html>
